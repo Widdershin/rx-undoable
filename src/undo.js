56967e0efdf2ccf418e335ae538b9aa42bf02179
Nick Johnstone <ncwjohnstone@gmail.com>
2015-10-20 08:40:38 +1300

    Write observable test for undo

--- src/undo.js
+++ src/undo.js
@@ -1,2 +1,5 @@
 export default function Undo (state$, undo$) {
+  return {
+    state$
+  };
 }
--- test/undo_test.js
+++ test/undo_test.js
@@ -4,6 +4,37 @@ import Rx from 'rx';
 import assert from 'assert';
 import Undo from '../src/undo';
 
+function createMessage(actual, expected) {
+  return 'Expected: [' + expected.toString() + ']\r\nActual: [' + actual.toString() + ']';
+}
+
+// Using QUnit testing for assertions
+var collectionAssert = {
+  assertEqual: function (expected, actual) {
+    var comparer = Rx.internals.isEqual,
+      isOk = true;
+
+    if (expected.length !== actual.length) {
+      assert(false, 'Not equal length. Expected: ' + expected.length + ' Actual: ' + actual.length);
+      return;
+    }
+
+    for(var i = 0, len = expected.length; i < len; i++) {
+      isOk = comparer(expected[i], actual[i]);
+      if (!isOk) {
+        break;
+      }
+    }
+
+    assert(isOk, createMessage(expected, actual));
+  }
+
+};
+
+const onNext = Rx.ReactiveTest.onNext,
+    onCompleted = Rx.ReactiveTest.onCompleted,
+    subscribe = Rx.ReactiveTest.subscribe;
+
 describe('Undo', () => {
   it('takes a stream of state and a stream of undo intent', (done) => {
     const state$ = Rx.Observable.just(
@@ -12,9 +43,39 @@ describe('Undo', () => {
 
     const undo$ = Rx.Observable.empty();
 
+    Undo(state$, undo$);
+
+    done();
+  });
+
+  it('undoes changes to state when asked', (done) => {
+    const scheduler = new Rx.TestScheduler();
+
+    const state$ = scheduler.createHotObservable(
+      onNext(100, {count: 0}),
+      onNext(200, {count: 1}),
+      onCompleted(500)
+    );
+
+    const undo$ = scheduler.createHotObservable(
+      onNext(300, true),
+      onCompleted(500)
+    );
+
     const undo = Undo(state$, undo$);
 
-    done()
+    collectionAssert.assertEqual(undo.state$.messages, [
+      onNext(100, {count: 0}),
+      onNext(200, {count: 1}),
+      onNext(300, {count: 0}),
+      onCompleted(500)
+    ]);
+
+    state$.onNext({count: 0});
+    state$.onNext({count: 1});
+    undo$.onNext(true);
+
+    done();
   });
-})
+});
 
